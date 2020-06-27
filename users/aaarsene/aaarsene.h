0810ba91787942c3f964d6ede078f2f372b15099
Arsène Thieffry <arsene.thieffry@gmail.com>
2020-06-27 19:50:44 +0200

    added my kyria keymap and userspace

---
+++ users/aaarsene/aaarsene.h
@@ -0,0 +1,13 @@
+#pragma once
+#include QMK_KEYBOARD_H
+
+#include "wrappers.h"
+
+/* Define layer names */
+enum userspace_layers {
+    _QWERTY  = 0,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+    _NAV
+};
