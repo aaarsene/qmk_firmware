90a687adf8eb5583f1abc4ef3b5542d099c6adcb
Arsène Thieffry <arsene.thieffry@gmail.com>
2020-12-29 13:51:31 +0100

    fix default keymap

--- keyboards/cc45/keymaps/default/keymap.c
+++ keyboards/cc45/keymaps/default/keymap.c
@@ -4,14 +4,14 @@ enum layers{
     _BASE,
     _NUM_SYM,
     _NAV_FN
-}
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, KC_BSPC,
         LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_NO,           KC_SPC,           KC_NO,           KC_NO, KC_RALT,   KC_RGUI, KC_RCTL
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          MO(_NAV_FN),
+        KC_LCTL, KC_LGUI, KC_LALT, KC_NO,           LT(_NUM_SYM,KC_SPC),           KC_NO,           KC_NO, KC_RALT,   KC_RGUI, KC_RCTL
     ),
     [_NUM_SYM] = LAYOUT(
         KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS, KC_DEL,
